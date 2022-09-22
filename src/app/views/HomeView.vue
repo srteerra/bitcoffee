7278d6b97eaa712b43c47fdf739982ca66e1b72a
Guapura89 <betocrack1228@gmail.com>
2022-09-21 20:05:41 -0600

    feat: Styles file added

--- src/app/views/HomeView.vue
+++ src/app/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <h1>Hello world</h1>
+    <b-container>
+      <div>
+        <h1>Your pag, your own place.</h1>
+        <p>Here with us</p>
+        <div>
+          <b-input-group prepend="Username" class="mt-3">
+            <b-form-input></b-form-input>
+            <b-input-group-append>
+              <b-button variant="outline-success">Button</b-button>
+              <b-button variant="info">Button</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </div>
+      </div>
+    </b-container>
   </div>
 </template>
 
