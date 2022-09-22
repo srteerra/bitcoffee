75ee950712b6437000946454fde8f10b3d0a5f35
Guapura89 <betocrack1228@gmail.com>
2022-09-21 19:33:31 -0600

    feat: Header and footer components added

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,15 +1,24 @@
 <!-- eslint-disable vue/attribute-hyphenation -->
 <template>
   <div>
-    <h1>Hello</h1>
+    <Header />
+    <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
   name: "App",
   data() {
     return {};
+  },
+  components: {
+    Header,
+    Footer,
   },
 };
 </script>
--- src/app/views/HomeView.vue
+++ src/app/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Jonocrod</h1>
+    <h1>Hello world</h1>
   </div>
 </template>
 
