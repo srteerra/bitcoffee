6f574efd56ffa7960dcc9cd969d75861aff926cb
Angel Lopez <srterradev@gmail.com>
2022-09-23 17:52:13 -0600

    feat: sanity schema and API sync

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-// const sanityClient = require("@sanity/client");
+const sanityClient = require("@sanity/client");
 
-// export const client = sanityClient({
-//   projectId: "pgbthv4x",
-//   dataset: "production",
-//   apiVersion: "2021-03-25", // use current UTC date - see "specifying API version"!
-//   token:
-//     process.env.SANITY_TOKEN ||
-//     "skBuFTldjifCi42ZehgQ7UaGIjS8dx120g7bbLKxKVA7TIyg8jmTl504LNPF99WZvdnhgZQfnbam1CS7uTiu7WMSdeURL3mILz2kUFv0GxTQt81xmjBCYQPndMC1G9XPL76r0cPyFEJvY0jNgSSu516nedaYLwmCD4hWXIpXp2YksMoy91nv", // or leave blank for unauthenticated usage
-//   useCdn: false, // `false` if you want to ensure fresh data
-// });
+export const client = sanityClient({
+  projectId: "v8g9fbqu",
+  dataset: "production",
+  apiVersion: "2021-03-25", // use current UTC date - see "specifying API version"!
+  token:
+    process.env.SANITY_TOKEN ||
+    "sk3VlDMVvEDI5AaWTeHKDktpyYqsvXaMAZQoFdVYQnMCXQ0tOv4kfJtss9X7b5iihjqmQORXXVvoi706UYj2NYg4XjjyDfZm96oCOFRsPZu65boNMVtvzuBKDU9PaTb6SrpxzJYec2YfWJqXeBSOcSbq2jmb9viv5RSsZbwK3yxG8fziEcmX", // or leave blank for unauthenticated usage
+  useCdn: false, // `false` if you want to ensure fresh data
+});
