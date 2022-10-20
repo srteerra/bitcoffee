e7d9b54847eacd12bb3721bd8b5a3addfa9f7417
Guapura89 <betocrack1228@gmail.com>
2022-10-20 00:38:53 -0600

    feat: Fix counter in goal card

--- src/app/components/UserGoalCard.vue
+++ src/app/components/UserGoalCard.vue
@@ -62,11 +62,25 @@
         </b-collapse>
 
         <!-- Counter -->
-        <h3><strong>8 days left</strong></h3>
+        <div v-if="!hide">
+          <div v-if="time">
+            <h3 v-if="DD <= 0" class="font-weight-bold">Only today</h3>
+            <h3 v-else class="font-weight-bold">{{ DD }} days left</h3>
+          </div>
+          <div v-else>
+            <h3 class="font-weight-bold">Finished</h3>
+          </div>
+        </div>
+        <div v-else>
+          <h3 v-if="time" id="timer" class="font-weight-bold">
+            {{ DD }} : {{ HR }} : {{ MN }} : {{ SC }}
+          </h3>
+          <h3 v-else class="font-weight-bold">Finished</h3>
+        </div>
 
         <!-- Time left collapse -->
         <b-collapse id="collapse-b" class="mt-2">
-          <b-card style="border: none">
+          <b-card :class="{ hide: !time }" style="border: none">
             <p>to complete the goal.</p>
           </b-card>
         </b-collapse>
@@ -117,14 +131,14 @@
         <b-collapse id="collapse-d" class="mt-2">
           <b-card style="border: none">
             <b-row align-h="center">
-              <b-col cols="4">
+              <b-col cols="12" md="4" class="my-2">
                 <b-button
                   class="w-100 mx-auto btn btn-dark font-weight-bold"
                   @click="claim"
                   >CLAIM</b-button
                 >
               </b-col>
-              <b-col cols="4">
+              <b-col cols="12" md="4" class="my-2">
                 <b-button
                   class="w-100 mx-auto font-weight-bold"
                   variant="outline-dark"
@@ -145,12 +159,12 @@
         <b-collapse id="collapse-a" class="mt-2">
           <b-card style="border: none">
             <b-row align-h="center">
-              <b-col cols="4">
+              <b-col cols="12" md="4" class="my-2">
                 <b-button class="btn btn-dark font-weight-bold w-100 mx-auto"
                   >CONTRIBUTE</b-button
                 >
               </b-col>
-              <b-col cols="4">
+              <b-col cols="12" md="4" class="my-2">
                 <b-button
                   class="w-100 mx-auto font-weight-bold"
                   variant="outline-dark"
@@ -182,10 +196,18 @@
 </template>
 
 <script>
+import { anyTypeAnnotation } from "@babel/types";
+
 export default {
   name: "UserGoalCard",
+
   data() {
     return {
+      DD: 0,
+      HR: 0,
+      MN: 0,
+      SC: 0,
+      time: true,
       goal_title: "A new acustic guitar",
       goal_status: 90,
       goal_description:
@@ -194,6 +216,8 @@ export default {
       categories: ["Music", "RIF"],
       blur: false,
       hide: false,
+      counter: 0,
+      finalDate: "October 28,2022 00:34:00",
     };
   },
   methods: {
@@ -206,6 +230,25 @@ export default {
     show() {
       this.hide = false;
     },
+    timer(date) {
+      let deadline = new Date(date).getTime();
+      let now = new Date().getTime();
+      let t = deadline - now;
+      this.DD = Math.floor(t / (1000 * 60 * 60 * 24));
+      this.HR = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.MN = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+      this.SC = Math.floor((t % (1000 * 60)) / 1000);
+      if (t <= 0) {
+        clearInterval(this.counter);
+        this.time = false;
+      }
+    },
+  },
+  created() {
+    var self = this;
+    self.counter = setInterval(function () {
+      self.timer(self.finalDate);
+    }, 1000);
   },
 };
 </script>
